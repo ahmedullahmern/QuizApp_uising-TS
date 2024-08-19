c206ab2d35ed57e913695a84984a9a3064c1c8b3
ahmedullah <ahmedullahmern@gmail.com>
2024-08-19 23:00:16 +0500

    new files bane he bhaiya

---
+++ dist/index.js
@@ -0,0 +1,64 @@
+"use strict";
+// Create an array of quiz questions using the interface
+const typeScriptQuizQuestions = [
+    {
+        question: "Which TypeScript feature allows you to define a type that is a combination of multiple types?",
+        options: [
+            "a) Union type",
+            "b) Intersection type",
+            "c) Tuple",
+            "d) Enum"
+        ],
+        answer: "b) Intersection type"
+    },
+    {
+        question: "What keyword is used to define a constant in TypeScript?",
+        options: [
+            "a) const",
+            "b) let",
+            "c) var",
+            "d) static"
+        ],
+        answer: "a) const"
+    },
+    {
+        question: "How can you specify the type of a function return value in TypeScript?",
+        options: [
+            "a) Use the `type` keyword",
+            "b) Use a type annotation after the function parameters",
+            "c) Use a type annotation after the function name",
+            "d) Use the `return` keyword"
+        ],
+        answer: "b) Use a type annotation after the function parameters"
+    },
+    {
+        question: "What is the purpose of the `never` type in TypeScript?",
+        options: [
+            "a) To represent a value that will never occur",
+            "b) To represent an empty object",
+            "c) To denote an uninitialized variable",
+            "d) To represent a type that can be any value"
+        ],
+        answer: "a) To represent a value that will never occur"
+    },
+    {
+        question: "Which of the following is used to define a class in TypeScript?",
+        options: [
+            "a) class",
+            "b) structure",
+            "c) type",
+            "d) object"
+        ],
+        answer: "a) class"
+    }
+];
+const question_html = document.querySelector("#question");
+const Chosse_answer = document.querySelector("#Chosse_answer");
+const next_btn = document.querySelector("#next_btn");
+let currentInd = 0;
+renderQiuzhtml();
+function renderQiuzhtml() {
+    const que = typeScriptQuizQuestions[currentInd];
+    question_html.innerText = `${currentInd + 1})${que.question}`;
+}
+console.log("typeScriptQuizQuestions", typeScriptQuizQuestions[currentInd]);
